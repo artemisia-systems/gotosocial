433e0e49ce581038a4d24d199223c2062c9f2d50
alas <alice@artemisia.systems>
2023-03-03 22:14:09 -0800

    Implement forced allowlist federation.

--- internal/federation/authenticate.go
+++ internal/federation/authenticate.go
@@ -48,6 +48,15 @@ type publicKeyer interface {
 	GetW3IDSecurityV1PublicKey() vocab.W3IDSecurityV1PublicKeyProperty
 }
 
+func isAllowlistedDomain(domain string) (bool) {
+    for _, v := range config.GetAllowedDomains() {
+        if v == domain {
+            return true
+        }
+    }
+    return false
+}
+
 /*
 getPublicKeyFromResponse is adapted from https://github.com/go-fed/apcore/blob/master/ap/util.go
 Thank you @cj@mastodon.technology ! <3
@@ -166,6 +175,13 @@ func (f *federator) AuthenticateFederatedRequest(ctx context.Context, requestedU
 		requestingRemoteAccount *gtsmodel.Account
 		requestingHost          = requestingPublicKeyID.Host
 	)
+
+    if !isAllowlistedDomain(requestingHost) {
+		err := errors.New("requesting instance not on allowlist.")
+		errWithCode := gtserror.NewErrorUnauthorized(err, err.Error())
+		log.Debug(ctx, errWithCode)
+		return nil, errWithCode
+    }
 
 	if host := config.GetHost(); strings.EqualFold(requestingHost, host) {
 		// LOCAL ACCOUNT REQUEST
